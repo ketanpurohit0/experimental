c081e976dafeb9f47ca73204fad1234c63828a17
ketan purohit <ketanpurohit0@gmail.com>
2020-05-27 14:30:46 +0100

    Added TAB

--- ReleaseSQL.ps1.ps1
+++ ReleaseSQL.ps1.ps1
@@ -7,7 +7,7 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $RootLevel                       = New-Object system.Windows.Forms.Form
-$RootLevel.ClientSize            = '908,729'
+$RootLevel.ClientSize            = '948,769'
 $RootLevel.text                  = "ETL Deployment GUI"
 $RootLevel.TopMost               = $false
 $RootLevel.add_Load({ OnFormLoad $this $_ })
@@ -121,7 +121,19 @@ $L_Message.location          = New-Object System.Drawing.Point(10,700)
 $L_Message.Font              = 'Microsoft Sans Serif,9'
 $L_Message.BackColor         = "LightBlue"
 
-$RootLevel.controls.AddRange(@($Label1,$Label2,$Label2a,$Label3,$L_Environment,$L_CHGTicket,$C_VersionList,$CheckBox1, $B_DeployVersion,$Label4,$DataGridView1,$L_Message))
+$TAB_Control 					= New-Object 'System.Windows.Forms.TabControl'
+$TAB_Control.Alignment			= "Top"
+$TAB_Control.SelectedIndex 		= 0
+$TAB_Control.Size				= "928,749"
+
+
+$TAB_Page_SQL 					= New-Object 'System.Windows.Forms.TabPage'
+$TAB_Page_SQL.Text 				= "SQL Release TAB"
+$TAB_Page_SQL.Size				= "928,749"
+
+$RootLevel.controls.Add($TAB_Control)
+$TAB_Control.controls.Add($TAB_Page_SQL)
+$TAB_Page_SQL.controls.AddRange(@($Label1,$Label2,$Label2a,$Label3,$L_Environment,$L_CHGTicket,$C_VersionList,$CheckBox1, $B_DeployVersion,$Label4,$DataGridView1,$L_Message))
 
 $C_VersionList.Add_SelectedIndexChanged({ OnSelectedIndexChanged $this $_ })
 $B_DeployVersion.Add_MouseClick({ OnDeployVersionMouseClick $this $_ })
