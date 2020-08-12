7f6f8d1f5df5b684d4a4caf65e5fdc97f8dd50d1
ketan purohit <ketanpurohit0@gmail.com>
2020-08-12 20:34:52 +0100

    Simple re-factoring by turning code into functions

--- WebScrape/WebScrape.py
+++ WebScrape/WebScrape.py
@@ -1,6 +1,28 @@
 import requests
 from  bs4 import BeautifulSoup
 
+
+def risers_fallers(URL):
+    rarr = []
+    page = requests.get(URL)
+    soup  = BeautifulSoup(page.content, "html.parser")
+    mt = soup.find('div', class_='market-table')
+    tbody= mt.find("tbody")
+    for t in tbody.find_all("tr"):
+        #print("*",t)
+        arr = []
+        elems = t.find_all("td")
+        for e in elems:
+            a  = e.find('a')
+            if (a is not None):
+                #print("@",a.attrs['href'])
+                arr.append(a.attrs['href'])
+            #print("@", e.text)
+            arr.append(e.text)
+        #print(arr)
+        rarr.append(arr)
+    return rarr
+
 # First page of risers and fallers (t-1)
 URL = "https://www.sharesmagazine.co.uk/market-scan/risers-fallers?page=1"
 # First page of volume spikes relative to monthly average
@@ -11,26 +33,5 @@ URL = "https://www.sharesmagazine.co.uk/market-scan/constant-risers-fallers?page
 URL = "https://www.sharesmagazine.co.uk/market-scan/highs-lows?page=1&highLow=high&period=monthly&filtersubmitbtn=Update"
 # First page of risers and fallers (t-4w)
 URL = "https://www.sharesmagazine.co.uk/market-scan/risers-fallers?dateComparison=2020-07-15&sortDirection=risers&index=&price=%2C&filtersubmitbtn=Update"
-
-page = requests.get(URL)
-
-#<div class="market-table">
-#<tbody>
-#<tr>
-#print(page.text)
-soup  = BeautifulSoup(page.content, "html.parser")
-mt = soup.find('div', class_='market-table')
-tbody= mt.find("tbody")
-for t in tbody.find_all("tr"):
-    #print("*",t)
-    arr = []
-    elems = t.find_all("td")
-    for e in elems:
-        a  = e.find('a')
-        if (a is not None):
-            #print("@",a.attrs['href'])
-            arr.append(a.attrs['href'])
-        #print("@", e.text)
-        arr.append(e.text)
-    print(arr)
-
+listOfRisersFallers = risers_fallers(URL)
+print(listOfRisersFallers)
--- WebScrape/WebScrapeFundamentals.py
+++ WebScrape/WebScrapeFundamentals.py
@@ -7,32 +7,34 @@ def optionalNumeric(s):
     except ValueError:
         return s
 
+
+
+def fundamentals(URL):
+
+    page = requests.get(URL)
+
+
+    soup  = BeautifulSoup(page.content, "html.parser")
+    mt = soup.find('table', class_='table-1 th-grey gutter-under-large fundamentalsTable vert-table')
+    map = {}
+    keys = []
+    values = []
+    for t in mt.find_all("tr"):
+        #print("*",t)
+        elems = t.children
+        for e in elems:
+            #print("*", e.name, e.string)
+            if (e.name == 'th'):
+                keys.append(e.string.strip(' \n\t'))
+            elif (e.name == 'td'):
+                values.append(e.string.strip(' \n\t'))
+    #print(keys, values)
+    zipped_kv =zip(keys, values)
+    for item in zipped_kv:
+        map[item[0]] = optionalNumeric(item[1])
+    return map
+
 # Fundamentals page
 URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/fundamentals"
-
-page = requests.get(URL)
-
-#<div class="market-table">
-#<tbody>
-#<tr>
-#print(page.text)
-soup  = BeautifulSoup(page.content, "html.parser")
-mt = soup.find('table', class_='table-1 th-grey gutter-under-large fundamentalsTable vert-table')
-map = {}
-keys = []
-values = []
-for t in mt.find_all("tr"):
-    #print("*",t)
-    elems = t.children
-    for e in elems:
-        print("*", e.name, e.string)
-        if (e.name == 'th'):
-            keys.append(e.string.strip(' \n\t'))
-        elif (e.name == 'td'):
-            values.append(e.string.strip(' \n\t'))
-print(keys, values)
-x=zip(keys, values)
-for item in x:
-    map[item[0]] = optionalNumeric(item[1])
-print(map)
-
+fundamentalsMap = fundamentals(URL)
+print(fundamentalsMap)
