33c87972376931f9e2e2bca07d2b722d885dc2f5
ketan purohit <ketanpurohit0@gmail.com>
2020-08-12 18:05:43 +0100

    WebScrape - POC extract fundamentals data on a ticket

--- WebScrape/WebScrapeFundamentals.py
+++ WebScrape/WebScrapeFundamentals.py
@@ -1,16 +1,14 @@
 import requests
 from  bs4 import BeautifulSoup
 
-# First page of risers and fallers (t-1)
-URL = "https://www.sharesmagazine.co.uk/market-scan/risers-fallers?page=1"
-# First page of volume spikes relative to monthly average
-URL = "https://www.sharesmagazine.co.uk/market-scan/volume-analysis?dateComparison=2&index=UKX&volumePctChange=%2C&price=%2C&pricePctChange=&sortBy=volumePctChange&sortDirection=volumeIncrease&filtersubmitbtn=Update"
-# First page of constant risers (3 consecutive days of rises)
-URL = "https://www.sharesmagazine.co.uk/market-scan/constant-risers-fallers?page=1&sortBy=risers&days=3&filtersubmitbtn=Update"
-# First page of monthly highs
-URL = "https://www.sharesmagazine.co.uk/market-scan/highs-lows?page=1&highLow=high&period=monthly&filtersubmitbtn=Update"
-# First page of risers and fallers (t-4w)
-URL = "https://www.sharesmagazine.co.uk/market-scan/risers-fallers?dateComparison=2020-07-15&sortDirection=risers&index=&price=%2C&filtersubmitbtn=Update"
+def optionalNumeric(s):
+    try:
+        return float(s)
+    except ValueError:
+        return s
+
+# Fundamentals page
+URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/fundamentals"
 
 page = requests.get(URL)
 
@@ -19,18 +17,22 @@ page = requests.get(URL)
 #<tr>
 #print(page.text)
 soup  = BeautifulSoup(page.content, "html.parser")
-mt = soup.find('div', class_='market-table')
-tbody= mt.find("tbody")
-for t in tbody.find_all("tr"):
+mt = soup.find('table', class_='table-1 th-grey gutter-under-large fundamentalsTable vert-table')
+map = {}
+for t in mt.find_all("tr"):
     #print("*",t)
-    arr = []
-    elems = t.find_all("td")
+    keys = []
+    values = []
+    elems = t.children
     for e in elems:
-        a  = e.find('a')
-        if (a is not None):
-            #print("@",a.attrs['href'])
-            arr.append(a.attrs['href'])
-        #print("@", e.text)
-        arr.append(e.text)
-    print(arr)
+        print("*", e.name, e.string)
+        if (e.name == 'th'):
+            keys.append(e.string.strip(' \n\t'))
+        elif (e.name == 'td'):
+            values.append(e.string.strip(' \n\t'))
+    print(keys, values)
+    x=zip(keys, values)
+    for item in x:
+        map[item[0]] = optionalNumeric(item[1])
+print(map)
 
