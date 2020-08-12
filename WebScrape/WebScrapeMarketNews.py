71207498ec236f777db473bc41e3cf5847a25188
ketan purohit <ketanpurohit0@gmail.com>
2020-08-12 21:10:05 +0100

    WebScrape  - add market news

---
+++ WebScrape/WebScrapeMarketNews.py
@@ -0,0 +1,33 @@
+import requests
+from  bs4 import BeautifulSoup
+
+def optionalNumeric(s):
+    try:
+        return float(s)
+    except ValueError:
+        return s
+
+
+
+def directorsDeals(URL):
+    rarr = []
+    keys = ['date','type', 'dir','pos', 'volp','value']
+    rarr.append(keys)
+    page = requests.get(URL)
+    soup  = BeautifulSoup(page.content, "html.parser")
+    mt = soup.find('table', class_='table-1 director-deals-sm')
+    tbody= mt.find("tbody")
+    for t in tbody.find_all("tr"):
+        values = []
+        elems = t.children
+        for e in elems:
+            if (e.name == 'td'):
+                values.append(e.text.strip(' \n\t'))
+        rarr.append(values)
+    return rarr
+
+if __name__ == "__main__":
+    # Fundamentals page
+    URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/director-deals"
+    directorsDealsList = directorsDeals(URL)
+    print(directorsDealsList)
