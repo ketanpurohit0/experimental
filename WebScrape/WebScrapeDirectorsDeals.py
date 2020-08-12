d14a6e05daebe2adcee288d04bfcd52a0889c2ff
ketan purohit <ketanpurohit0@gmail.com>
2020-08-12 21:01:06 +0100

    WebScrape - directors deals.

--- WebScrape/WebScrapeDirectorsDeals.py
+++ WebScrape/WebScrapeDirectorsDeals.py
@@ -9,33 +9,25 @@ def optionalNumeric(s):
 
 
 
-def fundamentals(URL):
-
+def directorsDeals(URL):
+    rarr = []
+    keys = ['date','type', 'dir','pos', 'volp','value']
+    rarr.append(keys)
     page = requests.get(URL)
-
-
     soup  = BeautifulSoup(page.content, "html.parser")
-    mt = soup.find('table', class_='table-1 th-grey gutter-under-large fundamentalsTable vert-table')
-    map = {}
-    keys = []
-    values = []
-    for t in mt.find_all("tr"):
-        #print("*",t)
+    mt = soup.find('table', class_='table-1 director-deals-sm')
+    tbody= mt.find("tbody")
+    for t in tbody.find_all("tr"):
+        values = []
         elems = t.children
         for e in elems:
-            #print("*", e.name, e.string)
-            if (e.name == 'th'):
-                keys.append(e.string.strip(' \n\t'))
-            elif (e.name == 'td'):
-                values.append(e.string.strip(' \n\t'))
-    #print(keys, values)
-    zipped_kv =zip(keys, values)
-    for item in zipped_kv:
-        map[item[0]] = optionalNumeric(item[1])
-    return map
+            if (e.name == 'td'):
+                values.append(e.text.strip(' \n\t'))
+        rarr.append(values)
+    return rarr
 
 if __name__ == "__main__":
     # Fundamentals page
-    URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/fundamentals"
-    fundamentalsMap = fundamentals(URL)
-    print(fundamentalsMap)
+    URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/director-deals"
+    directorsDealsList = directorsDeals(URL)
+    print(directorsDealsList)
