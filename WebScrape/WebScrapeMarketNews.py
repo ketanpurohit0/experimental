5b91cfb327682fa6055e55ad71b522cb8a5b43dc
ketan purohit <ketanpurohit0@gmail.com>
2020-08-12 21:42:25 +0100

    WebScrape - completed poc directors deals

--- WebScrape/WebScrapeMarketNews.py
+++ WebScrape/WebScrapeMarketNews.py
@@ -9,25 +9,28 @@ def optionalNumeric(s):
 
 
 
-def directorsDeals(URL):
+def marketNews(URL):
     rarr = []
-    keys = ['date','type', 'dir','pos', 'volp','value']
+    keys = ['date','news','src']
+    include_cols = "2,3,4"
     rarr.append(keys)
     page = requests.get(URL)
     soup  = BeautifulSoup(page.content, "html.parser")
-    mt = soup.find('table', class_='table-1 director-deals-sm')
-    tbody= mt.find("tbody")
-    for t in tbody.find_all("tr"):
+    mt = soup.find('table', class_='table-1')
+    for t in mt.find_all("tr"):
         values = []
         elems = t.children
+        i = 0
         for e in elems:
             if (e.name == 'td'):
-                values.append(e.text.strip(' \n\t'))
+                i=i+1
+                if ( include_cols.find(str(i)) ):
+                    values.append( e.text.strip(' \n\t'))
         rarr.append(values)
     return rarr
 
 if __name__ == "__main__":
-    # Fundamentals page
-    URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/director-deals"
-    directorsDealsList = directorsDeals(URL)
-    print(directorsDealsList)
+    # Market News page
+    URL = "https://www.sharesmagazine.co.uk/shares/share/7DIG/news/market"
+    marketNewsList = marketNews(URL)
+    print(marketNewsList)
