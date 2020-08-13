c189b720c4c100f396cf6ea76fd51a1adb71a51a
ketan purohit <ketanpurohit0@gmail.com>
2020-08-13 11:54:58 +0100

    Use 'smart' converter for datetime and float times. To standardize from 'Aug 12, 2020' to '2020-08-12T00:00:00'

--- WebScrape/WebScrapeHistoricalPrices.py
+++ WebScrape/WebScrapeHistoricalPrices.py
@@ -7,13 +7,14 @@ def historicalPrices(URL):
     rarr = []
     keys = ['date','close']
     include_cols = [1,5]
+    methodMap = dict({1: WSC.standardizeDate, 5: WSC.optionalNumeric})
     rarr.append(keys)
     page = requests.get(URL)
     soup  = BeautifulSoup(page.content, "html.parser")
     mt = soup.find('table', class_='footable-table table-1 gutter-under gutter-top-small')
     tbody = mt.find("tbody")
     for t in tbody.find_all("tr"):
-        rarr.append([WSC.optionalNumeric(t.select(f"td:nth-of-type({str(i)})")[0].text) for i in include_cols])       
+        rarr.append([methodMap[i](t.select(f"td:nth-of-type({str(i)})")[0].text) for i in include_cols])       
     return rarr
 
 if __name__ == "__main__":
